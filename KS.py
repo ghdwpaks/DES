c77a01b6ed542ec1b3bcab9abf0741fd84f27547
ghdwpaks <hjm7904@gmail.com>
2021-05-16 23:53:28 +0900

    key scheduling getting another key from pc 1 pc1이 키 적용 시스템에 사용될 키값 받아오기

--- KS.py
+++ KS.py
@@ -11,3 +11,28 @@ class KS :
         print(Key_scheduling_cryptogram)
         print("len(PCcryptogram) :",len(Key_scheduling_cryptogram))
         return Key_scheduling_cryptogram
+
+    def get_pc1() :
+        pc1_temp1 = []
+        f = open("PC1.txt", 'r')
+        while True:
+            line = f.readline()
+            if not line: break
+            pc1_temp1.append(line)
+        f.close()
+        print("pc1_temp1 :",pc1_temp1)
+        for i in range(len(pc1_temp1)) :
+            if pc1_temp1[i][-1:] == "\n" :
+                pc1_temp1[i] = pc1_temp1[i][:-1]
+        print()
+        print("pc1_temp1 :",pc1_temp1)
+        pc1_temp2 = []
+        for i in range(len(pc1_temp1)) :
+            pc1_temp2.append(pc1_temp1[i].split(","))
+        print("pc1_temp2 :",pc1_temp2)
+        count = 0
+        for i in range(len(pc1_temp2)) :
+            for j in range(len(pc1_temp2[i])) :
+                count += 1
+        print("count :",count)
+        return pc1_temp2
