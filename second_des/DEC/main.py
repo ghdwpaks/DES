d7b367b4fecf0bca6b790ca70c830080d3e6ce6d
ghdwpaks <hjm7904@gmail.com>
2021-05-25 03:50:22 +0900

    DEC IP1까지 자동 복호화 코딩 완료

--- second_des/DEC/main.py
+++ second_des/DEC/main.py
@@ -1,6 +1,7 @@
 import Functions as fc
 import F
 import Key_Scheduling as ks
+import copy as c
 ks.ks_main()
 IP_1_key = fc.get_txt_file_contents("Keys/IP-1.txt")
 IP_1_key = fc.set_2_dim_arr(IP_1_key)
@@ -15,19 +16,18 @@ fc.print_funcs.print_var_with_number("IP_1ed_inputs : {}".format(IP_1ed_inputs),
 fc.write_txt_file("Log_rerounds/R{}.txt".format(16),IP_1ed_inputs)
 
 
-
-R16 = fc.get_cry_left(IP_1ed_inputs)
-fc.print_funcs.print_var_with_number("R16 : {}".format(R16),4)
-L16_R15 = fc.get_cry_right(IP_1ed_inputs)
+cry = c.deepcopy(IP_1ed_inputs)
+R16 = fc.get_cry_left(cry)
+L16_R15 = fc.get_cry_right(cry)
 Fed_L16_R15 = F.F_main(L16_R15,16)
-fc.print_funcs.print_var_with_number("Fed_L16_R15 : {}".format(Fed_L16_R15),6)
 L15 = fc.CAL.Rebuild_XOR(R16,Fed_L16_R15)
-
 result = ''.join([L15,L16_R15])
 print("result :",result)
 fc.write_txt_file("Log_rerounds/R{}.txt".format(15),result)
 
-for i in range(14,0,-1) :
+
+
+for i in range(14,-1,-1) :
 
     cry = fc.get_txt_file_contents("Log_rerounds/R{}.txt".format(i+1))
     R1L2 = fc.get_cry_left(cry)
@@ -37,3 +37,11 @@ for i in range(14,0,-1) :
     result = ''.join([L1,R1L2])
     fc.write_txt_file("Log_rerounds/R{}.txt".format(i),result)
 
+IP1_key = fc.get_txt_file_contents("Keys/IP.txt")
+IP1_key = fc.set_2_dim_arr(IP1_key)
+
+r0 = fc.get_txt_file_contents("Log_rerounds/R0.txt")
+IP1ed_result = fc.DEC_apply_arr_to_cry(IP1_key,r0)
+print(IP1ed_result)
+#ENC : 1100111110100011001001110111111000011000011101011111001111100110
+#DEC : 1100111110100011001001110111111000011000011101011111001111100110
--- second_des/ENC/main.py
+++ second_des/ENC/main.py
@@ -6,7 +6,9 @@ import Key_Scheduling as Key_s
 Key_s.ks_main()
 main_cry = "ghdwpakss" #cryptogram 크립토그램, DES를 관통하는 암호문
 fir_of_fir_input = c.deepcopy(main_cry)
+
 main_cry = fc.STR2CRY(main_cry)
+fc.print_funcs.print_var_with_number(main_cry,123456789)
 print("\n\n")
 
 
